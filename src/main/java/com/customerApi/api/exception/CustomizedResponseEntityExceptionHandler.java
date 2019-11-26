4002c993463c027cfc903676d9a1dd7a66b71a50
Puja Singh <47050826+pujasingh21@users.noreply.github.com>
2019-11-25 23:12:44 -0500

    Update the Changes manually

--- src/main/java/com/customerApi/api/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/customerApi/api/exception/CustomizedResponseEntityExceptionHandler.java
@@ -3,17 +3,11 @@ package com.customerApi.api.exception;
 import java.util.Date;
 
 import org.hibernate.exception.JDBCConnectionException;
-<<<<<<< HEAD
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.CannotCreateTransactionException;
-=======
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
->>>>>>> d420947cdb09f4f696ee057b9392c79fbb730ce3
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -30,7 +24,6 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.NOT_FOUND);
 	}
 
-<<<<<<< HEAD
 	@ExceptionHandler(CannotCreateTransactionException.class)
 	public final ResponseEntity<ErrorDetails> handleDatabaseExceptions(CannotCreateTransactionException ex, WebRequest request) {
 		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getLocalizedMessage(),
@@ -40,10 +33,6 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	
 	@ExceptionHandler(EmptyResultDataAccessException.class)
 	public final ResponseEntity<ErrorDetails> handleDatabaseExceptions(EmptyResultDataAccessException ex, WebRequest request) {
-=======
-	@ExceptionHandler(JDBCConnectionException.class)
-	public final ResponseEntity<ErrorDetails> handleDatabaseExceptions(JDBCConnectionException ex, WebRequest request) {
->>>>>>> d420947cdb09f4f696ee057b9392c79fbb730ce3
 		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getLocalizedMessage(),
 				request.getDescription(false));
 		return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.NOT_FOUND);
