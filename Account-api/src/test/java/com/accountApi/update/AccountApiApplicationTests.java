41e653de35e8e5bfe2f8d8852629b4faaa613c62
kpuja <kpuja@gmail.com>
2019-11-30 17:31:21 -0500

    Added Account API

---
+++ Account-api/src/test/java/com/accountApi/update/AccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountApi.update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
